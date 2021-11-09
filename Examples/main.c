19266e85ddacf7997fcb88b807deded7243b022f
Guilherme Capucci <gscapucci@hotmail.com>
2021-11-09 03:02:05 -0300

    sub list exemples

--- Examples/main.c
+++ Examples/main.c
@@ -21,7 +21,7 @@ int main()
     LL_removeAtPosition(&list, 0);
     LL_print(&list);
 
-    LL_removeAtPosition(&list, list.lenght);
+    LL_removeAtPosition(&list, list.lenght - 1);
     LL_print(&list);
 
     LL_removeAtPosition(&list, list.lenght/2);
@@ -33,5 +33,18 @@ int main()
     LL_removeLast(&list);
     LL_print(&list);
 
+    LL_reverse(&list);
+    LL_print(&list);
+
+    LinkedList tsil = LL_subList(&list, 0, list.lenght/2);
+    LinkedList tsil2 = LL_subList(&list, list.lenght/2, list.lenght);
+
+    LL_print(&tsil);
+    LL_print(&tsil2);
+
+
     LL_clear(&list);
+    LL_clear(&tsil2);
+    LL_clear(&tsil);
+    return 0;
 }
