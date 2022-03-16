ff3d6064ac5cff87e5f28e3c512d0df6484e97c9
Guilherme Capucci <gscapucci@hotmail.com>
2022-03-15 23:49:47 -0300

    All

--- InOndeHeaderFile/DataStructures.h
+++
@@ -1,7 +0,0 @@
-#ifndef DATA_STRUCTURES_H
-#define DATA_STRUCTURES_H
-
-#include "../DataStructures/LinkedList/InOneHeaderFile/LinkedList.h"
-#include "../DataStructures/BinaryTree/InOneHeaderFile/BinaryTree.h"
-
-#endif /* DATA_STRUCTURES_H */
--- README.md
+++
@@ -1,2 +0,0 @@
-﻿# Linked list exemple:
- ![alt text](https://github.com/gscapucci/Data-structures-in-c/blob/main/Images/LinkedList-exemple-output.png)
