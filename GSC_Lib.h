49329247b554bdcf4653597b91a195a2173611a8
Guilherme Capucci <guilherme_capucci@outlook.com>
2022-03-21 12:36:04 -0300

    one include file for all lib

---
+++ GSC_Lib.h
@@ -0,0 +1,9 @@
+#ifndef GSC_LIB_H
+#define GSC_LIB_H
+
+#include "DataTypes/Dictionary.h"
+#include "DataTypes/String.h"
+#include "Stack.h" //stack includes Node.h and LinkedList.h
+#include "AVLTree.h" // avltree includes Node.h
+
+#endif /* GSC_LIB_H */
